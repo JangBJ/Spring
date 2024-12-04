e8cf8ef572a2ffbfd5aec2b0eddd227165c65052
JangBJ <qudwnd1001@naver.com>
2024-12-05 08:22:33 +0900

    자식

--- src/main/java/tobyspring/hellospring/Payment.java
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -1,4 +1,60 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+    private Long orderId;
+    private String currency;                    // 해당 통화에 대한 변환후 결제 금액
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;                  // 환률 계산
+    private BigDecimal convertedAmount;         // 환산된 금액
+    private LocalDateTime validUntil;           //한국에서만 서비스한다고 했을때의 계산 유효기간
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+         this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
+
+
--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,4 +1,26 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+
 public class PaymentService {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreginCurrencyAmount)throws Exception{
+        // 1. 환율 가져오기
+        URL url = new URL("https://open.er-api.com/v6/latest/USD" + currency);
+        HttpURLConnection connection =(HttpURLConnection) url.openConnection();
+        // 2. 금액 계산
+        // 3. 유효 시간 계산
+        return new Payment(orderId, currency, foreginCurrencyAmount, BigDecimal.ZERO,BigDecimal.ZERO, LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+
+
 }
+
